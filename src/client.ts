733d086118faa1d977354b7a86b3b563f2d42acc
changmyeong <changmyeong.baek@gmail.com>
2025-01-09 22:42:03 +0900

    feat: log error when error is not index_not_found_exception

--- src/client.ts
+++ src/client.ts
@@ -1,19 +1,19 @@
-import { Client, ClientOptions } from '@opensearch-project/opensearch';
-import { Model } from './model';
-import { indexMetadataMap } from './decorator';
-import { convertOptionsToMappingProperties } from './util';
+import { Client, ClientOptions } from "@opensearch-project/opensearch";
+import { Model } from "./model";
+import { indexMetadataMap } from "./decorator";
+import { convertOptionsToMappingProperties } from "./util";
 
 export let opensearchClient: Client;
 
 export const setOpenSearchClient = (options: ClientOptions) => {
   opensearchClient = new Client(options);
-}
+};
 
 export const initialize = async (
   opensearchClientOptions: ClientOptions,
   typensearchOptions?: {
     createIndexesIfNotExists?: Model[];
-  },
+  }
 ): Promise<void> => {
   opensearchClient = new Client(opensearchClientOptions);
 
@@ -34,23 +34,27 @@ export const initialize = async (
       };
     }
 
-    await opensearchClient.indices.get({ index: metadata.name }).catch((error) => {
-      if (error?.meta?.body?.error?.type === 'index_not_found_exception') {
-        opensearchClient.indices.create({
-          index: metadata.name,
-          body: {
-            settings: {
-              index: {
-                number_of_shards: metadata.numberOfShards,
-                number_of_replicas: metadata.numberOfReplicas,
-              }
+    await opensearchClient.indices
+      .get({ index: metadata.name })
+      .catch((error) => {
+        if (error?.meta?.body?.error?.type === "index_not_found_exception") {
+          opensearchClient.indices.create({
+            index: metadata.name,
+            body: {
+              settings: {
+                index: {
+                  number_of_shards: metadata.numberOfShards,
+                  number_of_replicas: metadata.numberOfReplicas,
+                },
+              },
+              mappings: {
+                properties: mappingProperties,
+              },
             },
-            mappings: {
-              properties: mappingProperties,
-            },
-          },
-        });
-      }
-    });
+          });
+        } else {
+          throw error;
+        }
+      });
   }
-}
+};
